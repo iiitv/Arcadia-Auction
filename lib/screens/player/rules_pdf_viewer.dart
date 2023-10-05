38300e0a2694b9791b7ce9c0272a31fd6b6bbe11
aspiringgarv <202251048@iiitvadodara.ac.in>
2023-10-05 14:01:14 +0530

    resolved error 22

--- lib/screens/player/rules_pdf_viewer.dart
+++ lib/screens/player/rules_pdf_viewer.dart
@@ -1,85 +1,152 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
 class RulesPdfViewer extends StatefulWidget {
   static const routeName = '/rules-pdf-viewer';
+
   @override
   _RulesPdfViewerState createState() => _RulesPdfViewerState();
 }
+
 class _RulesPdfViewerState extends State<RulesPdfViewer> {
-  PdfViewerController? _pdfViewerController;
+  late PdfViewerController _pdfViewerController;
+  late PdfTextSearchResult _searchResult;
+  TextEditingController searchcontroller = TextEditingController();
+
   @override
   void initState() {
     _pdfViewerController = PdfViewerController();
-    _pdfViewerController!.jumpToPage(1);
+    _searchResult = PdfTextSearchResult();
     super.initState();
   }
-  PdfTextSearchResult? _searchResult;
+
+  @override
+  void dispose() {
+    searchcontroller.dispose();
+    _pdfViewerController.dispose();
+    super.dispose();
+  }
+
+
+  void showNotFoundSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+
+        duration: Duration(milliseconds: 1000),
+        content: Text("Not Found"),
+
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    //  String pdfUrl = ModalRoute.of(context)!.settings.arguments as String;
+    Object? s = ModalRoute.of(context)?.settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text('Rules'),
+        title: const Text('Syncfusion Flutter PdfViewer'),
         actions: <Widget>[
-          // IconButton(
-          //   icon: Icon(
-          //     Icons.search,
-          //     color: Colors.white,
-          //   ),
-          //   onPressed: () async {
-          //     _searchResult = await _pdfViewerController?.searchText(
-          //       'the',
-          //       searchOption: TextSearchOption.caseSensitive,
-          //     );
-          //   },
-          // ),
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () async {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+
+                    title: Text('Search'),
+                    content: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                          ),
+                          controller: searchcontroller,
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        child: Text("Search"),
+                        onPressed: () async {
+                          _searchResult = await _pdfViewerController
+                              .searchText(searchcontroller.text.toString());
+                          if (kIsWeb) {
+                            setState(() {});
+                          } else {
+                            _searchResult.addListener(() {
+                              if (_searchResult.hasResult) {
+                                Navigator.maybePop(context);
+                                searchcontroller.clear();
+                                setState(() {});
+                              } else if (!_searchResult.hasResult) {
+                                Navigator.maybePop(context);
+                                searchcontroller.clear();
+                                setState(() {
+                                  showNotFoundSnackBar(context);
+                                });
+                              }
+                            });
+                          }
+                        },
+                      ),
+                    ],
+
+                  );
+                },
+              );
+            },
+          ),
           Visibility(
-            visible: _searchResult?.hasResult ?? false,
+            visible: _searchResult.hasResult,
             child: IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.clear,
                 color: Colors.white,
               ),
               onPressed: () {
                 setState(() {
-                  _searchResult!.clear();
+                  _searchResult.clear();
                 });
               },
             ),
           ),
           Visibility(
-            visible: _searchResult?.hasResult ?? false,
+            visible: _searchResult.hasResult,
             child: IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.keyboard_arrow_up,
                 color: Colors.white,
               ),
               onPressed: () {
-                _searchResult?.previousInstance();
+                _searchResult.previousInstance();
               },
             ),
           ),
           Visibility(
-            visible: _searchResult?.hasResult ?? false,
+            visible: _searchResult.hasResult,
             child: IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.keyboard_arrow_down,
                 color: Colors.white,
               ),
               onPressed: () {
-                _searchResult?.nextInstance();
+                _searchResult.nextInstance();
               },
             ),
           ),
         ],
       ),
-      body: Container(
-        // child: SfPdfViewer.network(
-        //   pdfUrl,
-        //   initialZoomLevel: 2,
-        // ),name
-        child: SfPdfViewer.asset('assets/docs/RuleBook.pdf'),
+      body: SfPdfViewer.asset(
+        'assets/docs/RuleBook.pdf',
+        controller: _pdfViewerController,
+        currentSearchTextHighlightColor: Colors.yellow.withOpacity(0.6),
+        otherSearchTextHighlightColor: Colors.yellow.withOpacity(0.3),
       ),
     );
   }
